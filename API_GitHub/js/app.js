9ac8a92b43ad69016e54822186e1546f626c0e88
Ezequiel Santos <ezequiel.mozart.2020@gmail.com>
2021-02-09 17:10:04 -0300

    adding search form

--- API_GitHub/js/app.js
+++ API_GitHub/js/app.js
@@ -1,10 +1,19 @@
+const form = document.querySelector('#form')
 async function getUserGithub(userName){
     const response = await fetch(`https://api.github.com/users/${userName}`); // faz requisição a API
     const { login, avatar_url } =  await response.json(); //transforma no formato json
-    
     
     document.querySelector('h2').innerHTML = login // inseri no h2 do HTML
     document.querySelector('img').src = avatar_url // inseri na img do HTML
    
 }
-getUserGithub('ezequielsan') // passando um nome de usuario como parâmetro
+ 
+form.addEventListener('submit', event => {
+
+    event.preventDefault()
+    
+    const nome = document.querySelector('#userName').value
+    console.log(nome)
+
+    getUserGithub(nome)
+})
--- documentation.md
+++
@@ -1,8 +0,0 @@
-## Integrando com a API do GitHub
-
- - minha primeira integração com API rest
- - Projeto para aprender a fazer integração com API's rest
- - Essa intergração busca os dados de um usuário no GitHub(que no caso é eu)
- 
-
-  
